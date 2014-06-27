e1e71d29c58f97a534bd044f9e05f7219d7fadcd
Richard Winters <rik@mmogp.com>
2014-06-27 12:14:11 -0400

    Project Update
    - Fixed require issue for NK in index.js
    
    Change-Id: I78591d1b6f4df0f27487246d3baf28db58375bfc
    Signed-off-by: Richard Winters <rik@mmogp.com>

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 /**
  * index.js
  * 
- * package: MMod-Node
+ * package: nk-mvc
  * version:  $id$
  * author:  Richard B. Winters <a href="mailto:rik@massivelymodified.com">rik At MassivelyModified</a>
- * copyright: 2013-2014 Richard B. Winters
+ * copyright: 2011-2014 Massively Modified, Inc.
  */
 
 
 // Deps
-var nk = require( '../nodakwaeri' ),
+var nk = require( 'nk' ),
 config = require( './config' ),
 app = new nk( config );
-//nodaklay = require( 'nodaklay' );
 
 
-// Entry point to our application
-app.init
-(
-	/*
-	 * To use a custom router, controller, renderer, etc, you can configure such here. You can alternatively customize the built in
-	 * MVC framework by specifying additional options here:
-	{	
-	 	routes: <routes_configuration>,							// OR
-	 	router: new nk.router()
-	 	controller_path: __dirname + '/app/controllers',		// OR
-		controller_provider: new nk.controller( { 'controller_path': __dirname + '/app/controllers' } ),
-		model_path: __dirname + '/app/models',					// OR
-		model_provider: new nk.model( { 'model_path': __dirname + '/app/models' } ),
-		view_path: __dirname + '/app/views',					// OR	
-		view_provider: new nodaklay.pottr( { 'view_path': __dirname + '/app/views' } ),
-		asset_path: __dirname + '/assets'	
-	}
-	*/
-);
+// Start our App
+app.init();
